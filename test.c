2f487667b4c833e9e2772f59dbb7b0208e45b28e
linted <linted@users.noreply.github.com>
2022-07-30 14:26:20 -0400

    update???

--- test.c
+++ test.c
@@ -1,16 +1,15 @@
 #include <msp430.h>
 #include <msp430g2121.h>
-// #include <legacymsp430.h>
 
 static void __attribute__((naked, section(".crt_0042"), used))
 disable_watchdog (void)
 {
-    // Unused pins as outputs
-    P1DIR = 0xFF;
-    P2DIR = 0xFF;
-
-    // IDK what this line is supposed to do, but it doesn't compile
+    // Disable the GPIO power-on default high-impedance mode to activate
+    // previously configured port settings
     // PM5CTL0 &= ~LOCKLPM5;
+    // Unused pins as outputs
+    P1DIR = 0xff;
+    P2DIR = 0xff;
 
     // idk what this does
     WDTCTL = WDTPW | WDTHOLD;
@@ -20,9 +19,11 @@ disable_watchdog (void)
     P2DIR |= BIT6 | BIT7;       //2.6 and 2.7 to output
 
     // Allow for I2C 
-    P1SEL |= BIT6 | BIT7;
-    P1OUT = BIT6 | BIT7;        // P1.6 & P1.7 Pullups
-    P1REN |= BIT6 | BIT7;       // P1.6 & P1.7 Pullups
+    P1SEL |= BIT1 | BIT4 | BIT6 | BIT7;
+    P2SEL |= BIT6 | BIT7;
+    // P1OUT = BIT6 | BIT7;        // P1.6 & P1.7 Pullups
+    P1REN |= BIT1 | BIT4 | BIT6 | BIT7;       // P1.6 & P1.7 Pullups
+    P2REN |= BIT6 | BIT7;
 
 }
 
@@ -31,19 +32,35 @@ void TIMER1_A0_ISR( void )
 {
     //Toggle P1.4
     P1OUT ^= BIT4;
-
 }
 
 
 int main(void) {
     // WDTCTL = WDTPW | WDTHOLD;
 
-    P1OUT |= BIT1; // turn on 1.1
+    P1OUT ^= BIT1 | BIT4; // turn on 1.1
 
     for (;;) {
-        P2OUT ^= BIT7; // swap 2.7
+        P1OUT ^= BIT1 | BIT4;
+        P2OUT ^= BIT7 | BIT6; // swap 2.7
 
-        __delay_cycles(200000); // sleep for 200000 cycles
+        __delay_cycles(900000); // sleep for 200000 cycles
     }
 
 }
+
+
+// int main(void)
+// {
+//   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+//   P1DIR |= BIT1 | BIT4;                     // P1.2 and P1.3 output
+//   P1SEL |= BIT1 | BIT4;                            // P1.2 and P1.3 TA1/2 options
+//   P2DIR |= BIT6 | BIT7;
+//   P2SEL |= BIT6 | BIT7;
+//   CCR0 = 512-1;                             // PWM Period
+//   CCTL1 = OUTMOD_7;                         // CCR1 reset/set
+//   CCR1 = 384;                               // CCR1 PWM duty cycle
+//   TACTL = TASSEL_1 + MC_1;                  // ACLK, up mode
+
+//   __bis_SR_register(LPM3_bits);             // Enter LPM3
+// }
